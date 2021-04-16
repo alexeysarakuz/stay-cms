9c188900494fa1fec312225dceb7c5cebc1aba59
Oleksii Sarakuz <oleksi.sarakuz@intl.shijigroup.com>
2021-04-16 15:50:39 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '38472f4ddaae66a1df24e80b51777de0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '92f96f4e-8546-45e9-a83a-7139cc7dec73'
+};
